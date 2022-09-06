b6146b6a81e4c976973cfe248f5269a85739c774
NakRakDo <kyunghun96@gmail.com>
2022-09-06 17:31:59 +0900

    Bj_4345 clear

--- Bj_4375.java
+++ Bj_4375.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Bj_4375 {
-    public static void main(String[] args) {
-        //모듈로 연산
-
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextInt()){
+            int num = scanner.nextInt(); //~~BufferedReader를 사용하려 했으나..~~
+            System.out.println(One(num));
+        }
     }
-
+    public static int One(int num) {
+        int x = 1;
+        for(int i=1; ; i++) {
+            x = x % num;
+            x = x * 10 +1;
+            if( x == 1) {
+                return i;
+            }
+        }
+    }
 
 }
