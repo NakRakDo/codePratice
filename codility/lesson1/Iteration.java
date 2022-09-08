d6fe43846a892c06266b91c8138c1f40814fe516
NakRakDo <kyunghun96@gmail.com>
2022-09-08 12:07:27 +0900

    Change Number type to cope with big number that excceeds 20digit or high

--- codility/lesson1/Iteration.java
+++ codility/lesson1/Iteration.java
@@ -1,57 +1,130 @@
 package codility.lesson1;
+import java.math.BigInteger;
 
 public class Iteration {
     public static void main(String[] args) {
-        //System.out.println(Math.pow(2,0));
-        System.out.println(getBinaryNum(62));
-        System.out.println(getBinaryNum(63));
-        System.out.println(getBinaryNum(38));
-        System.out.println(getBinaryNum(16));
-        System.out.println(getBinaryNum(2));
-        System.out.println(getBinaryNum(4));
-        System.out.println(getBinaryNum(8));
-        System.out.println(getBinaryNum(32));
-        System.out.println(getBinaryNum(15));
-        System.out.println(getBinaryNum(7));
 
+        System.out.println("Binary Num : " + getBinaryNum(561892));
+        System.out.println("Gap : " + calculateGap(getBinaryNum(561892)));
+    }
+    public static int calculateGap(BigInteger N) {
+
+        char[] binaryNumToString = String.valueOf(N.toString()).toCharArray();
+
+        int prevIndex = 0;
+        int nextIndex = 0;
+        int gap = 0;
+
+        for(int i = 0; i < binaryNumToString.length; i++){
+            if(binaryNumToString[i] == '1'){
+                prevIndex = nextIndex;
+                nextIndex = i;
+            }
+            if(((nextIndex-1) - prevIndex) > 0){
+                gap = ((nextIndex-1) - prevIndex) >= gap ? ((nextIndex-1) - prevIndex) : gap;
+            }
+        }
+
+        if(gap <= 0) return 0;
+        return gap;
     }
 
-    public static int Solution(int N) {
+    public static BigInteger getBinaryNum(int N) {
+        BigInteger bigN = BigInteger.valueOf(N);
+        int index = 1;
+        if(N <= 1) return bigN;
+        while(true){
+
+            if((( bigN.compareTo(BigInteger.valueOf(2).pow(index)) == 1
+                    || bigN.compareTo(BigInteger.valueOf(2).pow(index)) ==0 ))
+                    && bigN.compareTo(BigInteger.valueOf(2).pow(index+1)) ==-1 ){
+                break;
+            }
+            index++;
+        }
+        BigInteger binaryNum = BigInteger.valueOf(10).pow(index);
+        BigInteger temp = bigN.subtract(BigInteger.valueOf(2).pow(index));
+
+        for(int i = index-1; i > 0; i--){
+            if(i == 1) {
+                if((temp.subtract(BigInteger.valueOf(2).pow(i)).equals(BigInteger.valueOf(0)))){
+                    binaryNum = binaryNum.add(BigInteger.valueOf(10).pow(i));
+                }else if((temp.subtract(BigInteger.valueOf(2).pow(i)).equals(BigInteger.valueOf(1)))){
+                    binaryNum = binaryNum.add(BigInteger.valueOf(10).pow(i)).add(BigInteger.valueOf(1));
+                }else if(temp.equals(1)){
+                    binaryNum = binaryNum.add(BigInteger.valueOf(1));
+                }
+            }else{
+                if((temp.subtract(BigInteger.valueOf(2).pow(i)).compareTo(BigInteger.valueOf(0))) == 1 ||
+                      (temp.subtract(BigInteger.valueOf(2).pow(i)).compareTo(BigInteger.valueOf(0))) == 0){
+                    binaryNum = binaryNum.add(BigInteger.valueOf(10).pow(i));
+                    temp = temp.subtract(BigInteger.valueOf(2).pow(i));
+                }
+            }
+        }
+        return binaryNum;
+    }
+}
 
 
+/*
+import java.util.*;
 
-        return 0;
+class Solution {
+    public int solution(int N) {
+        if(N > 2147483647 && N < 1) return -1;
+        return caculateGap(getBinaryNum(N));
     }
 
-    public static int getBinaryNum(int N) {
+    public int caculateGap(long N) {
+        char[] binaryNumToString = String.valueOf(N).toCharArray();
+        int prevIndex = 0;
+        int nextIndex = 0;
+        int gap = 0;
+
+        for(int i = 0; i < binaryNumToString.length; i++){
+            if(binaryNumToString[i] == '1'){
+                prevIndex = nextIndex;
+                nextIndex = i;
+            }
+            if(((nextIndex-1) - prevIndex) > 0){
+                gap = ((nextIndex-1) - prevIndex) >= gap ? ((nextIndex-1) - prevIndex) : gap;
+            }
+        }
+        if(gap <= 0) return 0;
+        return gap;
+    }
+
+    public long getBinaryNum(int N){
         int index = 1;
         if(N <= 1) return N;
         while(true){
-            if((Math.pow(2,index) <= N && (Math.pow(2,(index+1))) > N)) break;
+            if(((long)Math.pow(2,index) <= N && ((long)Math.pow(2,(index+1)))>N)) break;
             index++;
         }
 
-        double BinaryNum = Math.pow(10,index);
-        double temp = N - Math.pow(2,index);
-
+        long binaryNum = (long)Math.pow(10,index);
+        long temp = N - (long)Math.pow(2,index);
 
         for(int i = index-1; i > 0; i--){
-            if(i == 1){
-                if((temp - (Math.pow(2,i)) == 0)){
-                    BinaryNum += Math.pow(10,i);
-                }else if((temp - (Math.pow(2,i)) == 1)){
-                    BinaryNum += Math.pow(10,i)+1;
+            if(i == 1) {
+                if((temp - ((long)Math.pow(2,i)) == 0)){
+                    binaryNum += (long)Math.pow(10,i);
+                }else if((temp - ((long)Math.pow(2,i)) == 1)){
+                    binaryNum += (long)Math.pow(10,i)+1;
+                }else if(temp == 1){
+                    binaryNum += 1;
                 }
                 break;
             }else{
-                if((temp - (Math.pow(2,i)) >= (Math.pow(2,(i-1))))){
-                    BinaryNum += Math.pow(10,i);
-                    temp -= Math.pow(2,i);
+                if((temp - ((long)Math.pow(2,i)) >= 0)){
+                    binaryNum += (long)Math.pow(10,i);
+                    temp -= (long)Math.pow(2,i);
                 }
             }
-
         }
-        return (int)BinaryNum;
+        return binaryNum;
     }
-
 }
+
+* */
